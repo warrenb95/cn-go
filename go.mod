779440943bb04c5575a1cbe52296c14b90a588cc
Warren <warrenb95@live.co.uk>
2024-09-07 22:46:59 +0100

    super simple api

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
 module github.com/warrenb95/cn-go
 
-go 1.22.0
+go 1.23.0
 
-require (
-	github.com/stretchr/testify v1.9.0
-	github.com/tj/assert v0.0.3
-)
+require github.com/stretchr/testify v1.9.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,81 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"io"
+	"log"
 	"net/http"
-	"time"
+
+	"github.com/warrenb95/cn-go/internal/store"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.PathValue("key")
+	if key == "" {
+		http.Error(w, "missing key ", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	defer log.Println(r.Body.Close())
+	if err != nil {
+		http.Error(w, "failed to read body", http.StatusInternalServerError)
+		return
+	}
+
+	err = store.Put(key, string(body))
+	if err != nil {
+		http.Error(w, "failed to save key: value", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.PathValue("key")
+	if key == "" {
+		http.Error(w, "missing key", http.StatusBadRequest)
+		return
+	}
+
+	value, err := store.Get(key)
+	if err != nil {
+		if errors.Is(err, store.ErrKeyNotFound) {
+			http.Error(w, "key value not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "can't get key value", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write([]byte(value))
+	if err != nil {
+		http.Error(w, "failed to write key value", http.StatusInternalServerError)
+		return
+	}
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.PathValue("key")
+	if key == "" {
+		http.Error(w, "missing key", http.StatusBadRequest)
+		return
+	}
+
+	err := store.Delete(key)
+	if err != nil {
+		http.Error(w, "failed to delete key value", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("PUT /v1/{key}", putHandler)
+	mux.HandleFunc("GET /v1/{key}", getHandler)
+	mux.HandleFunc("DELETE /v1/{key}", deleteHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
