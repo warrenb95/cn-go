458d699d66dec347a758973e50153dc9e58fee1a
Warren <warrenb95@live.co.uk>
2024-09-08 12:20:07 +0100

    db mutex's

--- internal/store/map.go
+++ internal/store/map.go
@@ -3,22 +3,34 @@ package store
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
-var store = make(map[string]string)
+type mapStore struct {
+	sync.RWMutex
+	m map[string]string
+}
+
+var ms = mapStore{
+	m: make(map[string]string),
+}
 
 var (
 	ErrKeyNotFound = errors.New("key not found")
 )
 
 func Put(key, value string) error {
-	store[key] = value
+	ms.Lock()
+	defer ms.Unlock()
+	ms.m[key] = value
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	v, ok := store[key]
+	ms.RLock()
+	defer ms.RUnlock()
+	v, ok := ms.m[key]
 	if !ok {
 		return "", fmt.Errorf("getting from store: %w", ErrKeyNotFound)
 	}
@@ -27,7 +39,9 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	ms.Lock()
+	defer ms.Unlock()
+	delete(ms.m, key)
 
 	return nil
 }
